f1e82ec1d0c58a00b83925e519f348ab1109e1cc
Khoa Dang Anh <anhkhoa1597@yahoo.com.vn>
2025-06-17 15:07:20 +0700

    Implement searchGift controller with filters for name, category, condition, and age_years using query parameters

--- giftlink-backend/src/controllers/searchController.js
+++ giftlink-backend/src/controllers/searchController.js
@@ -2,27 +2,25 @@ import { NotFoundError } from "../middlewares/errorHandler.js";
 import Gift from "../models/gift.js";
 import logger from "../utils/logger.js";
 
-export const getAllGifts = async (req, res, next) => {
+export const searchGift = async (req, res, next) => {
   try {
-    const gifts = await Gift.find({});
-    logger.info("Fetch all Gift");
-    res.json(gifts);
-  } catch (error) {
-    logger.error("Error fetching gifts", { stack: error.stack });
-    next(error);
-  }
-};
-
-export const getGiftById = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const gift = await Gift.findOne({ id });
-    if (!gift) {
+    let query = {};
+    const name = req.query.name;
+    if (name && name.trim() !== "")
+      query.name = { $regex: name, $options: "i" };
+    if (req.query.category)
+      query.category = { $regex: req.query.category, $options: "i" };
+    if (req.query.condition)
+      query.condition = { $regex: req.query.condition, $options: "i" };
+    if (req.query.age_years)
+      query.age_years = { $lte: parseInt(req.query.age_years) };
+    const gifts = await Gift.find(query);
+    if (gifts.length === 0) {
       logger.warn("Gift not found");
       throw new NotFoundError("Gift not found");
     }
-    logger.info(`Fetched gift with id ${id}`);
-    res.json(gift);
+    logger.info(`Fetched gifts successfully`);
+    res.json(gifts);
   } catch (error) {
     logger.error("Error", { stack: error.stack });
     next(error);
--- giftlink-backend/src/routes/search.routes.js
+++ giftlink-backend/src/routes/search.routes.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { searchGift } from "../controllers/searchController.js";
 const router = express.Router();
+
+router.get("/", searchGift);
 
 export default router;
