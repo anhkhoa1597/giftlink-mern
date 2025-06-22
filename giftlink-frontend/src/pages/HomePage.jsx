774b9f5398ab5b3d0ac508b0234ef59270af314a
Khoa Dang Anh <anhkhoa1597@yahoo.com.vn>
2025-06-22 14:24:23 +0700

    design HomePage

--- giftlink-frontend/src/pages/HomePage.jsx
+++ giftlink-frontend/src/pages/HomePage.jsx
@@ -3,7 +3,17 @@ import "./HomePage.css";
 const HomePage = () => {
   return (
     <div className="container">
-      <h1>Home Page</h1>
+      <div className="content">
+        <h1>GiftLink</h1>
+        <h2>Share Gifts and Joy!</h2>
+        <p className="lead">
+          "Sharing is the essence of community. It is through giving that we
+          enrich and perpetuate both our lives and the lives of others."
+        </p>
+        <a href="/main" className="btn btn-primary">
+          Get Started
+        </a>
+      </div>
     </div>
   );
 };
