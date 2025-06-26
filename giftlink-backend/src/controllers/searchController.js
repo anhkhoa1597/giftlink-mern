8b04bed37ac7020ec490d4c79cf7fef4426fcdbe
Khoa Dang Anh <anhkhoa1597@yahoo.com.vn>
2025-06-26 13:54:17 +0700

    refactor searchGift function; implement pagination and improve error handling

--- giftlink-backend/src/controllers/searchController.js
+++ giftlink-backend/src/controllers/searchController.js
@@ -5,22 +5,36 @@ import logger from "../utils/logger.js";
 export const searchGift = async (req, res, next) => {
   try {
     let query = {};
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 12;
+    const skip = (page - 1) * limit;
+
     const name = req.query.name;
     if (name && name.trim() !== "")
       query.name = { $regex: name, $options: "i" };
-    if (req.query.category)
+    if (req.query.category && req.query.category !== "")
       query.category = { $regex: req.query.category, $options: "i" };
-    if (req.query.condition)
+    if (req.query.condition && req.query.condition !== "")
       query.condition = { $regex: req.query.condition, $options: "i" };
-    if (req.query.age_years)
+    if (req.query.age_years && req.query.age_years > 0)
       query.age_years = { $lte: parseInt(req.query.age_years) };
-    const gifts = await Gift.find(query);
+
+    const [gifts, total] = await Promise.all([
+      Gift.find(query).skip(skip).limit(limit),
+      Gift.countDocuments(query),
+    ]);
+
     if (gifts.length === 0) {
-      logger.warn("Gift not found");
-      throw new NotFoundError("Gift not found");
+      throw new NotFoundError("Item not found");
     }
-    logger.info(`Fetched gifts successfully`);
-    res.json(gifts);
+
+    logger.info(`Fetched searching gifts successfully`);
+    res.json({
+      gifts,
+      total,
+      page,
+      totalPages: Math.ceil(total / limit),
+    });
   } catch (error) {
     logger.error("Error", { stack: error.stack });
     next(error);
